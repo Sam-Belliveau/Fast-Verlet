0097866cc02f30d184b979731be7c9a4135b1c3e
Sam Belliveau <sam.belliveau@gmail.com>
2025-10-17 15:18:56 -0400

    Change resolution

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ extern crate sfml;
 
 pub mod constants;
 
-pub mod stopwatch;
 pub mod particle;
+pub mod stopwatch;
 
 pub mod simulation;
 
@@ -11,11 +11,11 @@ use std::f64::consts::PI;
 
 use constants::*;
 
-use simulation::Simulator;
 use particle::Particle;
- 
-use sfml::graphics::{RenderWindow, RenderTarget, Color};
-use sfml::window::{Style, Event, mouse};
+use simulation::Simulator;
+
+use sfml::graphics::{Color, RenderTarget, RenderWindow};
+use sfml::window::{mouse, Event, Style};
 
 fn color(t: &mut f64) -> Color {
     *t += 3.88322208;
@@ -28,22 +28,26 @@ fn color(t: &mut f64) -> Color {
 
 fn main() {
     let mut window = RenderWindow::new(
-        (3600, 2400),
+        (800, 600),
         "SFML Example",
-        Style::CLOSE,
+        Style::DEFAULT,
         &Default::default(),
-    ).expect("Cannot create a new Render Window");
+    )
+    .expect("Cannot create a new Render Window");
 
     const FPS: u32 = 120;
 
     window.set_framerate_limit(FPS);
 
-    let mut simulation = Simulator::new(Vec2::new(3600.0, 2400.0), Box::new(|_|{
-        // let offset = particle.pos - Vec2::new(1800.0, 1200.0);
+    let mut simulation = Simulator::new(
+        Vec2::new(window.size().x as f64, window.size().y as f64),
+        Box::new(|_| {
+            // let offset = particle.pos - Vec2::new(1800.0, 1200.0);
+            // offset / offset.length_sq().max(10.0) * -10000000.0
 
-        // offset / offset.length_sq().max(10.0) * -10000000.0
-        Vec2::new(0.0, 500.0)
-    }));
+            Vec2::new(0.0, 500.0)
+        }),
+    );
 
     let mut fc = 0.0;
     let mut p = 0;
@@ -52,7 +56,11 @@ fn main() {
         while let Some(event) = window.poll_event() {
             match event {
                 Event::Closed => window.close(),
-                Event::KeyPressed { .. } => {fc = 0.0; p = 0; simulation.clear();},
+                Event::KeyPressed { .. } => {
+                    fc = 0.0;
+                    p = 0;
+                    simulation.clear();
+                }
                 _ => (),
             }
         }
@@ -64,21 +72,18 @@ fn main() {
 
         if !b && mouse::Button::Left.is_pressed() {
             b = true;
-            let m : Vec2 = window.mouse_position().as_other();
+            let m: Vec2 = window.mouse_position().as_other();
             for x in -10..=10 {
                 for y in -10..=10 {
                     fc += 0.001;
                     p += 1;
                     let r = 6.0;
-                    simulation.add(Box::new(
-                        Particle::new(
-                            m + Vec2::new(x as f64, -y as f64) * (2.0 * r),
-                            Vec2::new(100.0, 0.0) * r,
-                            r,
-                            color(&mut (1.0 * fc))
-                        )
-                    )
-                    );
+                    simulation.add(Box::new(Particle::new(
+                        m + Vec2::new(x as f64, -y as f64) * (2.0 * r),
+                        Vec2::new(100.0, 0.0) * r,
+                        r,
+                        color(&mut (1.0 * fc)),
+                    )));
                 }
             }
         } else {
